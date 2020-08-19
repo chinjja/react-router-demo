8167110f0d886c833c386b84c924962e587fdd4d
chinjja <chinjja@gmail.com>
2020-08-19 21:50:29 +0900

    Code Splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Route } from 'react-router-dom';
-import { Home, About, Posts } from './pages';
 import Menu from './components/Menu';
 import ShowPageInfo from './components/ShowPageInfo';
+
+const Home = React.lazy(() => import('./pages/Home'));
+const About = React.lazy(() => import('./pages/About'));
+const Posts = React.lazy(() => import('./pages/Posts'));
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Menu/>
-        <Route exact path='/' component={Home}/>
-        <Route path='/about/:name?' component={About}/>
-        <Route path="/posts" component={Posts}/>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Route exact path='/' component={Home}/>
+          <Route path='/about/:name?' component={About}/>
+          <Route path="/posts" component={Posts}/>
+        </Suspense>
         <ShowPageInfo/>
       </div>
     );
