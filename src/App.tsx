7b55c019a7955f62c7193971ec327356f1879efe
chinjja <chinjja@gmail.com>
2020-08-20 17:17:47 +0900

    Revert "Code Splitting"
    This reverts commit 8167110f0d886c833c386b84c924962e587fdd4d.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import { Route } from 'react-router-dom';
+import { Home, About, Posts } from './pages';
 import Menu from './components/Menu';
 import ShowPageInfo from './components/ShowPageInfo';
-
-const Home = React.lazy(() => import('./pages/Home'));
-const About = React.lazy(() => import('./pages/About'));
-const Posts = React.lazy(() => import('./pages/Posts'));
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Menu/>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Route exact path='/' component={Home}/>
-          <Route path='/about/:name?' component={About}/>
-          <Route path="/posts" component={Posts}/>
-        </Suspense>
+        <Route exact path='/' component={Home}/>
+        <Route path='/about/:name?' component={About}/>
+        <Route path="/posts" component={Posts}/>
         <ShowPageInfo/>
       </div>
     );
