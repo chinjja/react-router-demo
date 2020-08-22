858c9e69adba531abec8df3e6dba5433ed2c097d
chinjja <chinjja@gmail.com>
2020-08-22 15:21:33 +0900

    Integration with react-router

--- src/components/ShowPageInfo.tsx
+++ src/components/ShowPageInfo.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
-import { Breadcrumbs, makeStyles, Theme, createStyles, Chip } from '@material-ui/core';
+import { withRouter, Link as RouterLink } from 'react-router-dom';
+import { Breadcrumbs, makeStyles, Theme, createStyles, Chip, Link, Typography, LinkProps } from '@material-ui/core';
 import { NavigateNext, Home } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme: Theme) => 
@@ -18,15 +18,36 @@ const useStyles = makeStyles((theme: Theme) =>
         }
     })
 );
+
+interface LinkRouterProps extends LinkProps {
+    to: string;
+    replace?: boolean;
+}
+
+const LinkRouter = (props: LinkRouterProps) => <Link {...props} component={RouterLink}/>;
+
 const ShowPageInfo = withRouter(({ match, location }) => {
     const classes = useStyles();
-    const paths = "Home" + location.pathname;
-    const contents = paths.split('/').map((value, index) => (
-        value ? <Chip key={index} label={value} onDelete={()=>{}} icon={<Home className={classes.icon}/>}/> : null
-    ));
+    const paths = location.pathname.split('/').filter(x => x);
     return (
-        <Breadcrumbs aria-label='breadcrumb' separator={<NavigateNext/>} className={classes.breacrumb}>
-            {contents}
+        <Breadcrumbs aria-label='breadcrumb' separator={<NavigateNext />} className={classes.breacrumb}>
+            <LinkRouter color='inherit' to='/'>
+                Home
+            </LinkRouter>
+            {paths.map((value, index) => {
+                const last = paths.length - 1 === index;
+                const to = '/' + paths.slice(0, index + 1).join('/');
+
+                return last ? (
+                    <Typography color='textPrimary' key={to}>
+                        {value}
+                    </Typography>
+                ) : (
+                        <LinkRouter color='inherit' to={to} key={to}>
+                            {value}
+                        </LinkRouter>
+                    )
+            })}
         </Breadcrumbs>
     )
 });
