b7e14faac5c7bd3a06b7eae165cfab428849a589
chinjja <chinjja@gmail.com>
2020-08-24 11:42:46 +0900

    Using websocket

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import axios from 'axios';
 import { Typography, Button, Paper } from '@material-ui/core';
 import {} from '@material-ui/icons';
+import ws from 'websocket';
+
+const apiUrl = 'http://localhost:8888';
+const wsUrl = 'ws://localhost:8888/rooms';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8888',
+    baseURL: apiUrl,
     timeout: 1000
 });
 
@@ -18,8 +22,18 @@ interface RoomState {
 }
 
 export default class Room extends React.Component<{}, RoomState> {
+    client?: ws.w3cwebsocket;
+
     componentDidMount() {
         this.updateData();
+        this.client = new ws.w3cwebsocket(wsUrl);
+        this.client.onmessage = (e) => {
+            this.updateData();
+        }
+    }
+
+    componentWillUnmount() {
+        this.client?.close(1000, `Room's component will unmount`);
     }
 
     async fetchData() {
@@ -36,18 +50,36 @@ export default class Room extends React.Component<{}, RoomState> {
         });
     }
 
+    deleteRoom(id: string) {
+        console.log('id? ' + id);
+        api.delete('/rooms/'+id).then(() => {
+            this.updateData();
+        })
+    }
+
     render() {
         const { status, rooms } = this.state || {};
         const items = rooms?.map((room)=>(
             <Paper key={room.id} variant='outlined'>
                 <Typography>ID: {room.id}</Typography>
                 <Typography>Title: {room.title}</Typography>
+                <Button
+                    variant='outlined'
+                    onClick={()=>this.deleteRoom(room.id!)}
+                >
+                    Delete
+                </Button>
             </Paper>
         ));
         return (
             <Paper variant='outlined'>
                 <Typography>Status: {status}</Typography>
-                <Button onClick={()=>this.updateData()}>Refresh</Button>
+                <Button
+                    variant='outlined'
+                    onClick={()=>this.updateData()}
+                >
+                    Refresh
+                </Button>
                 {items}
             </Paper>
         );
